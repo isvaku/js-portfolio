02c5cbd1256dd5e628d7da5d312323bcd9071fd5
Rigoberto Quezada <isvaku@hotmail.com>
2021-12-21 21:30:44 -0600

    Add bundle analyzer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,9 +3,9 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const Dotenv = require('dotenv-webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
-  watch: true,
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -13,6 +13,7 @@ module.exports = {
     assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   mode: "development",
+  devtool: 'source-map',
   resolve: {
     extensions: [".js"],
 	alias: {
@@ -72,6 +73,14 @@ module.exports = {
         },
       ],
     }),
-	new Dotenv()
-  ]
+	new Dotenv(),
+  new BundleAnalyzerPlugin(),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
+    open: true
+  }
 };
